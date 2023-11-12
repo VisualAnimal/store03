e2fdd2e20552250a5592157fb0fef6dbaaa5c6d4
unknown <visualanimal@icloud.com>
2023-11-12 13:11:37 +0800

    测试fetch api能不能用

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [jsonData, setJsonData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // 本地 JSON 文件路径或远程 JSON URL
+        const jsonUrl = 'data.json';
+
+        // 使用 fetch 获取 JSON 数据
+        const response = await fetch(jsonUrl);
+        const data = await response.json();
+
+        // 更新组件状态
+        setJsonData(data);
+      } catch (error) {
+        console.error('Error fetching JSON data:', error);
+      }
+    };
+
+    // 调用数据获取函数
+    fetchData();
+  }, []); // 依赖数组为空表示只在组件加载时调用 useEffect
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>JSON Data</h1>
+      <ul>
+        {jsonData.map((item, index) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
